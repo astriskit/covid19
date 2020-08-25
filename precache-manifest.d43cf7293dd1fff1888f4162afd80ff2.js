cbc0fbeec28b572752a7072dcdffe90095bd068f
astriskit <harry2office@gmail.com>
2020-08-25 19:01:00 +0530

    added prod build

---
+++ precache-manifest.d43cf7293dd1fff1888f4162afd80ff2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f56c0c855437baecc5e80e90dac97b1f",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "05c5c1a0b49771e39014",
+    "url": "/covid19/static/css/2.97ca6098.chunk.css"
+  },
+  {
+    "revision": "6653f7b8a6bed277226d",
+    "url": "/covid19/static/css/main.c86fc453.chunk.css"
+  },
+  {
+    "revision": "05c5c1a0b49771e39014",
+    "url": "/covid19/static/js/2.06b18571.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/covid19/static/js/2.06b18571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6653f7b8a6bed277226d",
+    "url": "/covid19/static/js/main.82348121.chunk.js"
+  },
+  {
+    "revision": "816c657adb2b094ae332",
+    "url": "/covid19/static/js/runtime-main.f771569c.js"
+  }
+]);
